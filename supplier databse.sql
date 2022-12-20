254094bb3da0e93ebc2dac90413c5ccd0ddb02b0
Nithin-47 <100336015+Nithin-47@users.noreply.github.com>
2022-12-20 13:35:21 +0530

    Update supplier databse.sql

--- supplier databse.sql
+++ supplier databse.sql
@@ -1,4 +1,5 @@
 create database if not exists Supplier_database;
+drop database Supplier_database;
 use Supplier_database;
 
 -- creating Supplier table --
@@ -23,18 +24,17 @@ select * from parts;
 create table Catalog(
  sid int,
  pid int,
- cost int,
- foreign key(pid) references parts(pid),
- foreign key(sid) references Supplier(sid)
+ cost real,
+ foreign key(pid) references parts(pid) on delete cascade,
+ foreign key(sid) references Supplier(sid) on delete cascade
 )
 select * from parts;
 -- inserting into Supplier table --
 insert into Supplier 
-values('10001','Eren Yeager','Bangalore'),
-	  ('10002','Levi Ackerman','Kolkatta'),
-      ('10003','Tenzen Uzui','Mumbai'),
-      ('10004','Erwin Smith','Delhi'),
-      ('10005','Rengoku Kyojuro','Chennai');
+values('10001','Basavaraj Chitoor','Bangalore'),
+	  ('10002','Aadarsh Hosahalli','Kolkatta'),
+      ('10003','Chatrapathi Brijesh','Mumbai'),
+      ('10004','Kaarthik Sharma','Delhi');
 select * from Supplier;
 
 -- inserting into parts table --
@@ -48,35 +48,58 @@ select * from parts;
 
 -- inserting into Catalog table --
 insert into Catalog 
-values ('10001','20001','30'),
+values ('10001','20001','10'),
        ('10001','20002','10'),
-       ('10001','20003','10'),
-       ('10001','20004','50'),
-       ('10001','20005','20'),
-       ('10002','20004','50'),
-       ('10002','20002','15'),
-       ('10003','20003','10'),
-       ('10004','20003','15');
+       ('10001','20003','30'),
+       ('10001','20004','10'),
+       ('10001','20005','10'),
+       ('10002','20001','10'),
+       ('10002','20002','20'),
+       ('10003','20003','30'),
+       ('10004','20003','40');
 select * from Catalog;
  
  
 
---     week 7 todo queries   --
+-- --     week 7 todo queries   --
 
- #todo 3:
- 
- select distinct(p.pname)
- from parts p,Catalog c
- where p.pid=c.pid;
- 
- #todo 4:
-  -- before running query delete a tuple from table parts so that query gives output --
-  --  delete from parts  --
-  --  where pid='20006';  --
-  
-  select (s.sname)
-  from Supplier s,Catalog c
-  where s.sid=c.sid and c.pid=all(select pid
-								   from parts);
-                                   
-                                   
+--  #todo 3:
+--  
+--  select distinct(p.pname)
+--  from parts p,Catalog c
+--  where p.pid=c.pid;
+--  
+--  #todo 4:
+--   -- before running query delete a tuple from table parts so that query gives output --
+--   --  delete from parts  --
+--   --  where pid='20006';  --
+--   
+--   select (s.sname)
+--   from Supplier s,Catalog c
+--   where s.sid=c.sid and c.pid=all(select pid
+-- 								   from parts);
+
+
+select distinct P.pname from parts P,Catalog C where C.sid=some(select sid from Catalog group by pid );
+
+select S.sname from Supplier S where S.sid in (select C.sid from Catalog C group by C.sid having count(distinct C.pid)=(select count(*) from parts));
+
+
+
+-- select S.sname from Supplier S where S.sid in(select C.sid from Catalog C group by C.sid having count(distinct C.pid)>=(select count(*) from parts where color='Red'));
+
+-- select S.sname from Supplier S where S.sid in(select C.sid from Catalog C where C.pid 
+
+select distinct(sname) from Supplier S where not exists(select p.pid,p.color from parts p where not exists(select * from Catalog C where C.sid=S.sid and p.pid=C.pid) and p.color='Red');
+
+select P.pname from parts P where P.pid in(select C.pid from Catalog C where C.sid=10001 and C.pid not in(select ca.pid from Catalog ca where ca.sid!=10001)); 
+
+
+select S.sid from Supplier S where S.sid in (select C.sid from Catalog C where C.cost>(select avg(C1.cost) from Catalog C1 where C1.pid=C.pid group by pid));
+
+
+-- select S.sname from Supplier S where S.sid in (select C.sid from Catalog C group by C.pid having max(C.cost));
+
+
+
+select c.pid,s.sname from Supplier s,Catalog c where c.sid=s.sid and c.cost=(select max(c1.cost) from Catalog c1 where c1.pid=c.pid group by c1.pid);
